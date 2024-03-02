1ba13b2be42db8105b0c84939696ab99dbf6c19d
Fery Ramadhan <117572105+feryrmdhn@users.noreply.github.com>
2024-03-02 10:57:55 +0800

    Update auto_predict_service.py

--- app/services/auto_predict_service.py
+++ app/services/auto_predict_service.py
@@ -2,13 +2,13 @@ from fastapi import HTTPException, Depends
 from app.utils.api_utils import validate_api_key
 from joblib import load
 import psycopg2
+import os
+
+print("Current working directory:", os.getcwd())
+
+# Adjust the import path based on the current working directory
 import sys
-
-# Get the project directory
-project_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Add the project directory to the system path
-sys.path.append(project_dir)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app.db.connection import postgreSQL_connection
 
